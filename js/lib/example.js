63006e9d5dd56f51c65cbed3fa1e3d9b3b711312
Megan Wu <meganwu1994@gmail.com>
2020-04-29 13:58:40 -0500

    windows platform package.json file command line added

--- js/lib/example.js
+++ js/lib/example.js
@@ -22,6 +22,7 @@ require('./style.css')
 
 // When serialiazing the entire widget state for embedding, only values that
 // differ from the defaults will be specified.
+// is this updating, how about now, and now
 let MultiviewModel = widgets.DOMWidgetModel.extend({
     defaults: _.extend(widgets.DOMWidgetModel.prototype.defaults(), {
         _model_name : 'MultiviewModel',
