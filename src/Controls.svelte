6c16661db0debeca8a68ca66968fe7dcb867c8fb
Sam Lee <samanthanlee7@gmail.com>
2021-07-27 13:05:28 -0400

    updated the design for the waveform

--- src/Controls.svelte
+++ src/Controls.svelte
@@ -6,7 +6,6 @@
 	export let velocity;
 	export let position;
 	export let volume=1;
-	export let updateZoom;
 
 	export let toggleHideSaved;
 	export let hideSaved;
@@ -98,6 +97,5 @@
 	</select>
 	<span>{new Date(position*1000).toISOString().substr(11, 8)}</span>
 	<div style="display: inline-block;" on:mouseover={()=>{volHidden=false;}} on:mouseout={()=>{volHidden=true;}}>🔈 <div class="popup" class:hidden={volHidden}><input type="range" min="0" max="1" step="0.01" bind:value={volume}><span>{volume}</span></div></div>
-	<div style="display: inline-block;" on:mouseover={()=>{zoomHidden=false;}} on:mouseout={()=>{zoomHidden=true;}}>🔍 <div class="popup" class:hidden={zoomHidden}><input on:mouseup={()=>{updateZoom(pxSec)}} type="range" min="0" max="500" step="1" bind:value={pxSec}><span>{pxSec}</span></div></div>
 	<label>hide saved <input type="checkbox" bind:checked={hideSaved} on:change={toggleHideSaved} /></label>
 </div>
