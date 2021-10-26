3c55206edcb2669aee00ae6f25ddad4224a7d610
imandel <im334@cornell.edu>
2021-10-26 15:12:32 -0400

    refactor

--- src/widget.ts
+++ src/widget.ts
@@ -11,16 +11,16 @@ import { MODULE_NAME, MODULE_VERSION } from './version';
 
 import App from './App.svelte';
 
-export class ExampleModel extends DOMWidgetModel {
+export class MapView extends DOMWidgetModel {
   defaults() {
     return {
       ...super.defaults(),
-      _model_name: ExampleModel.model_name,
-      _model_module: ExampleModel.model_module,
-      _model_module_version: ExampleModel.model_module_version,
-      _view_name: ExampleModel.view_name,
-      _view_module: ExampleModel.view_module,
-      _view_module_version: ExampleModel.view_module_version,
+      _model_name: MapView.model_name,
+      _model_module: MapView.model_module,
+      _model_module_version: MapView.model_module_version,
+      _view_name: MapView.view_name,
+      _view_module: MapView.view_module,
+      _view_module_version: MapView.view_module_version,
     };
   }
 
@@ -29,15 +29,15 @@ export class ExampleModel extends DOMWidgetModel {
     // Add any extra serializers here
   };
 
-  static model_name = 'ExampleModel';
+  static model_name = 'MapView';
   static model_module = MODULE_NAME;
   static model_module_version = MODULE_VERSION;
-  static view_name = 'ExampleView'; // Set to null if no view
+  static view_name = 'ViewMap'; // Set to null if no view
   static view_module = MODULE_NAME; // Set to null if no view
   static view_module_version = MODULE_VERSION;
 }
 
-export class ExampleView extends DOMWidgetView {
+export class ViewMap extends DOMWidgetView {
   render() {
     new App({
       target: this.el,
